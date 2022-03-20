94ad10b31754061ae8a03a0132ba9515d53948b7
PaulS <psoliz333@gmail.com>
2022-03-20 18:32:29 -0500

    added apod api to index.html

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,16 @@
+var apiKey = 'C3Y2n0r4MS4rDGTcmc2tBopQ0tq65lTPdkk9aVS2'
 
+var apod = function () {
+    var apodApi = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey;
+    fetch(apodApi).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                $('.apodImg').attr('src', data.url)
+                console.log(data)
+                console.log(data.url)
+            })
+        }
+    })
+}
 
+apod();
