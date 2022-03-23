162b1a66e0fa0685f3090ab50620bfb8dc300fe6
Steffen568 <sgonzalez568@gmail.com>
2022-03-23 11:42:10 -0500

    test please ignore

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,25 @@ var apod = function () {
 }
 
 apod();
-var infoContainerElement = document.querySelector('#infoContainer')
+
+function fetchData() {
+    //check to if document contains a div with the specified ID and display the correct js to the HTML page
+    if(document.getElementById("astronautInfoContainer")) {
+        astronautData()
+    }
+    if(document.getElementById("launchInfoContainer")) {
+        upcomingLaunchData()
+    }
+    if(document.getElementById("stationInfoContainer")) {
+        fetchIssData()
+    }
+}
+
+// $('#launchCard').click(upcomingLaunchData())
+// $('#stationCard').click(fetchIssData())
+
+
+var infoContainerElement = document.querySelector('#launchInfoContainer')
  
    
  // FETCH API FOR UPCOMING LAUNCHES
@@ -50,9 +68,10 @@ var displayLaunchInfo = function (upcomingLaunch) {
 
         // create container card for upcomingLaunch info
         var infoDivElement = document.createElement('div')
-        infoDivElement.classList = 'w3-container w3-center w3-border w3-border-orange w3-dark-gray w3-round-xxlarge'
+        infoDivElement.classList = 'w3-container w3-center w3-border w3-border-orange w3-dark-gray w3-round-xxlarge w3-margin'
         // append div to parent container
-        document.getElementById("launchInfoContainer").appendChild(infoDivElement)
+        infoContainerElement.appendChild(infoDivElement)
+        
 
         //  create element to hold image
         var upcomingImageElement = document.createElement('img')
@@ -82,6 +101,8 @@ var displayLaunchInfo = function (upcomingLaunch) {
         infoDivElement.appendChild(upcomingTimerElement)
     }
 }
+
+
      
  
  // FETCH API FOR ASTRONAUT INFO
@@ -101,15 +122,7 @@ function astronautData() {
     })
 }
 
-function fetchData() {
-    //check to if document contains a div with the specified ID and display the correct js to the HTML page
-    if(document.getElementById("astronautInfoContainer")) {
-        astronautData()
-    }
-    if(document.getElementById("launchInfoContainer")) {
-        upcomingLaunchData()
-    }
-}
+
 
 
 
@@ -158,7 +171,7 @@ var displayAstronautData = function (astronauts) {
     }
 }
 
-fetchData()
+
 
 // function fetchData() {
 //     //check to if document contains a div with the specified ID
@@ -173,3 +186,100 @@ fetchData()
 //          // code for astronautData() goes here
 //      }
 // }
+
+
+// ISS SECTION
+var stationContainerEl = document.querySelector('#stationInfoContainer')
+var issLiveStreamEl = document.querySelector('#liveStream')
+
+
+function fetchIssData() {
+    var apiURL = `https://lldev.thespacedevs.com/2.2.0/spacestation/4/`
+
+    fetch(apiURL).then(function (response) {
+        if (response.ok) {
+            console.log(response)
+            response.json().then(function (data) {
+                console.log(data)
+                displayIssInfo(data)
+            })
+        } else {
+            alert('ERROR!')
+        }
+    })
+}
+
+
+var displayIssInfo = function(issData) {
+    issLiveStreamEl.classList.remove('hide')
+    // formatt data form api fetch
+    var issImage = issData.image_url
+    var issName = issData.name
+    var issDescription = issData.description
+    var issOrbit = issData.orbit
+    var issCrew = issData.onboard_crew
+    var issHeight = issData.height
+    var isswidth = issData.width
+
+    // create dive and append to stationContainer
+    var issDivEl = document.createElement('div')
+    issDivEl.classList = 'w3-container w3-center w3-border w3-border-orange w3-dark-gray w3-round-xxlarge' 
+   stationContainerEl.appendChild(issDivEl)
+    stationContainerEl.classList = 'w3-container w3-center'
+    
+
+    // create and append station headline
+    var issNameEl = document.createElement('h2')
+    issNameEl.textContent = issName
+    issDivEl.appendChild(issNameEl)
+
+    // create and append station image
+    var issImageEl = document.createElement('img')
+    issImageEl.src = issImage
+    issImageEl.classList = 'w3-round w3-image'
+    issImageEl.height = 700
+    issImageEl.width = 700
+    issDivEl.appendChild(issImageEl)
+
+    // create and append station discription 
+    var issDescriptionEl = document.createElement('p')
+    issDescriptionEl.textContent = issDescription
+    issDivEl.appendChild(issDescriptionEl)
+
+    // append live stream
+    issDivEl.appendChild(issLiveStreamEl)
+
+    // create new div for ISS info
+    var issInfoDiv = document.createElement('div')
+    issInfoDiv.classList = 'w3-container w3-center'
+    stationContainerEl.appendChild(issInfoDiv)
+
+    // create and append orbit button
+    var issOrbitEl = document.createElement('btn')
+    issOrbitEl.textContent = `Orbit: ${issOrbit}`
+    issOrbitEl.classList = 'w3-button w3-round w3-orange'
+    issInfoDiv.appendChild(issOrbitEl)
+
+    // create and append crew button
+    var issCrewEl = document.createElement('btn')
+    issCrewEl.textContent = `Onboard Crew: ${issCrew}`
+    issCrewEl.classList = 'w3-button w3-round w3-blue'
+    issInfoDiv.appendChild(issCrewEl)
+
+    // create and append height button
+    var issHeightEl = document.createElement('btn')
+    issHeightEl.textContent = `Height: ${issHeight}ft`
+    issHeightEl.classList = 'w3-button w3-round w3-red'
+    issInfoDiv.appendChild(issHeightEl)
+
+    // create and append width button
+    var issWidthEl = document.createElement('btn')
+    issWidthEl.textContent = `Width: ${isswidth}ft`
+    issWidthEl.classList = 'w3-button w3-round w3-red'
+    issInfoDiv.appendChild(issWidthEl)
+
+}
+
+fetchData()
+
+
